60654e15ebfd633640ddab574ce132d9c598edcc
vitali <vitali.mog@gmail.com>
2019-08-12 21:10:13 +0300

    addes firefox download folder support

--- lib/kimurai/browser_builder/selenium_firefox_builder.rb
+++ lib/kimurai/browser_builder/selenium_firefox_builder.rb
@@ -26,6 +26,7 @@ module Kimurai::BrowserBuilder
         driver_options.profile = Selenium::WebDriver::Firefox::Profile.new
         driver_options.profile["browser.link.open_newwindow"] = 3 # open windows in tabs
         driver_options.profile["media.peerconnection.enabled"] = false # disable web rtc
+        driver_options.log_level = :debug
 
         # Proxy
         if proxy = @config[:proxy].presence
